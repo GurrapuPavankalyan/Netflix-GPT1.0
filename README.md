959e147ef3e51d0ee3fcb594f5c5229cd21f7bab
GurrapuPavanKalyan <pavankalyan.gurram@123>
2024-10-23 23:38:38 +0530

    TMDB API data fetch && built MainContainer

--- README.md
+++ README.md
@@ -3,8 +3,35 @@
 - Create React App
 - Configured TailwindCSS
 - Header
+- Routing of App
 - Login Form
 - Signup Form
+- Form Validation using Regex
+- useRef Hook
+- Firebase Setup
+- Deploying our app to production
+- Create SignUp user account
+- Implemented sign In user API from firebase
+- Created Redux store with userSlice
+- Impemented Sign Out
+- Update Profile
+- BugFix: Signup user displayName and profile picture update
+- BugFix: If the user is not logged in Redirect/browse to Login Page and vice-versa
+- Unsubscribed to the onAuthStateChanged callback
+- Add hardcoded values to the constants file
+- BugFix Pending: Fix the user-Icon in Browse page.
+- Register TMDB API & create an app & get access token
+- Get Data from TMDB now playing movies list API
+- Custom Hook for Now Playing Movies
+- Create movieSlice
+- Update Store with movies Data
+- Planning for MainContainer & secondary container
+- Fetch Data for Trailer Video
+- Update Store with Trailer video Data
+- Embedded the Youtube video and make it autoplay  and mute
+- Tailwind Classes to make Main Container look awesome
+
+
 
 
 # Shortcuts
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector(store => store.user);
+
+  const handleSignOut = () =>{
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate("/error");
+    }).catch((error) => {
+      // An error happened.
+    });
+  };
+
+    useEffect(() =>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // User is signed in
+             const {uid, email, displayName, photoURL} = user;
+             dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));       
+            } else {
+              // User is signed out
+             dispatch(removeUser());
+            }
+          });
+    }, [dispatch]);
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
         <img 
         className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
         alt="logo"
          />
+        {user && (<div className="flex p-2">
+          <img 
+          className="w-12 h-12"
+          alt='user-icon'
+          src={user.photoURL}
+          />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            sign Out
+          </button>
+        </div>
+        )}
     </div>
   );
 };
